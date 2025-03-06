9efb908bb594a9acfd35d396f747680d831d2e69
Kathleen Garrity <kathleen.garrity223@gmail.com>
2025-03-05 22:59:55 -0500

    feat: add swanson pyramid viz

---
+++ src/visualizations/swanson-pyramid/data.ts
@@ -0,0 +1,228 @@
+export const pyramidData = [
+  {
+    row: 1,
+    title: "Honor",
+    description: "If you need it defined, you don't have it.",
+    color: "#fff",
+  },
+  //
+  {
+    row: 2,
+    title: "America",
+    description:
+      'The only country that matters. If you want to experience other "cultures" use an atlas or a ham radio.',
+    color: "#83a675",
+  },
+  {
+    row: 2,
+    title: "Buffets",
+    description: "Whenever available. Choose quanityt of quality.",
+    color: "#9cdbda",
+  },
+  //
+  { row: 3, title: "Weapons", description: "", color: "#fbd9aa" },
+  { row: 3, title: "Wood Working", description: "", color: "#fdf5a1" },
+  { row: 3, title: "Welfare Avoidance", description: "", color: "#c3aa66" },
+  //
+  {
+    row: 4,
+    title: "Teamwork",
+    description: "Work toegther as if your life depends on it...IT DOES!",
+    color: "#fdf5a1",
+  },
+  {
+    row: 4,
+    title: "Selfishness",
+    description: "Take what's yours.",
+    color: "#e99677",
+  },
+  {
+    row: 4,
+    title: "Haircuts",
+    description: "3 acceptable styles: High and Tight, Crew Cut & Buzz Cut.",
+    color: "#f6e3c1",
+  },
+  {
+    row: 4,
+    title: "Greatness Itself",
+    description: "The best revenge.",
+    color: "#fbd9aa",
+  },
+  //
+  {
+    row: 5,
+    title: "Discipline",
+    description: "The ability to repeat a boring thing over and over again.",
+    color: "#9cdbda",
+  },
+  {
+    row: 5,
+    title: "Attire",
+    description:
+      'Shorts over 6" are capri pants. Shorts under 6" are European.',
+    color: "#fdf5a1",
+  },
+  {
+    row: 5,
+    title: "Self-Reliance",
+    description: "Trust yourself",
+    color: "#d0b48e",
+  },
+  {
+    row: 5,
+    title: "Suspicion",
+    description: "Do not trust anyone else.",
+    color: "#f6e3c1",
+  },
+  {
+    row: 5,
+    title: "Skim Milk",
+    description: "That's right. It's on here twice. Avoid it.",
+    color: "#e99677",
+  },
+  //
+  { row: 6, title: "Cow Protein", description: "", color: "#fdf5a1" },
+  { row: 6, title: "Pig Protein", description: "", color: "#d0b48e" },
+  { row: 6, title: "Chicken Protein", description: "", color: "#f6e3c1" },
+  { row: 6, title: "Romantic Love", description: "", color: "#fdf5a1" },
+  { row: 6, title: "Deer Protein", description: "", color: "#e99677" },
+  { row: 6, title: "Fish (Sport Only)", description: "", color: "#fbd9aa" },
+  //
+  {
+    row: 7,
+    title: "Intensity",
+    description: "Give 100%. 110% is impossible. Only idiots recommend that.",
+    color: "#d0b48e",
+  },
+  {
+    row: 7,
+    title: "Torso",
+    description: "Should be thick and impenetrable.",
+    color: "#e99677",
+  },
+  {
+    row: 7,
+    title: "Old Wooden Sailing Shipts",
+    description: "They're beautiful.",
+    color: "#c3aa66",
+  },
+  {
+    row: 7,
+    title: "B.O.",
+    description: "Cultivating a manly musk puts your oppponents on notice.",
+    color: "#e99677",
+  },
+  {
+    row: 7,
+    title: "Stillness",
+    description: "Don't waste energy moving unless necessary.",
+    color: "#83a675",
+  },
+  { row: 7, title: "Skim Milk", description: "Avoid it.", color: "#f6e3c1" },
+  {
+    row: 7,
+    title: "Cursing",
+    description:
+      "There's only one bad word: Taxes. If any other word is good enough for sailors, it's good enough for you.",
+    color: "#fdf5a1",
+  },
+  //
+  {
+    row: 8,
+    title: "Friends",
+    description: "One to three is sufficient",
+    color: "#f6e3c1",
+  },
+  {
+    row: 8,
+    title: "Property Rights",
+    description: "They exist. Do not let them be taken away from you.",
+    color: "#d0b48e",
+  },
+  {
+    row: 8,
+    title: "Masonry",
+    description:
+      "Building walls makes you strong. Defending them makes you even stronger.",
+    color: "#9cdbda",
+  },
+  {
+    row: 8,
+    title: "Cabins",
+    description: "A place ot rest is made of logs.",
+    color: "#83a675",
+  },
+  {
+    row: 8,
+    title: "Perspiration",
+    description:
+      "Only sweat during physical activity or love making. No emotional sweating.",
+    color: "#fdf5a1",
+  },
+  {
+    row: 8,
+    title: "You",
+    description: "You are your biggest ally.",
+    color: "#f6e3c1",
+  },
+  {
+    row: 8,
+    title: "Crying",
+    description: "Acceptable at funerals and the Grand Canyon.",
+    color: "#9cdbda",
+  },
+  { row: 8, title: "Physical Fitness", description: "", color: "#f6e3c1" },
+  //
+  { row: 9, title: "Frankness", description: "Cut the B.S.", color: "#9ad5da" },
+  {
+    row: 9,
+    title: "Capitalism",
+    description: "God's way of determining who is smart, and who is poor.",
+    color: "#c3aa66",
+  },
+  {
+    row: 9,
+    title: "Facial Hair",
+    description:
+      "Full, thick and square. Nothing sculpted. If you have to sculpt it, that probably means you can't grow it.",
+    color: "#fdf5a1",
+  },
+  {
+    row: 9,
+    title: "Living in the Woods",
+    description: "Live off the land",
+    color: "#e99677",
+  },
+  {
+    row: 9,
+    title: "Rage",
+    description:
+      "One rage every three months is permitted. Try not to hurt anyone who doesn't deserve it.",
+    color: "#fbd9aa",
+  },
+  {
+    row: 9,
+    title: "Security",
+    description: "Secure the land.",
+    color: "#9cdbda",
+  },
+  {
+    row: 9,
+    title: "Poise",
+    description:
+      "Sting like a bee. Do not float like a butterfly. That's ridiculous.",
+    color: "#d0b48e",
+  },
+  {
+    row: 9,
+    title: "Handshakes",
+    description: "Firm, Dry, Solid. 3 seconds.",
+    color: "#9cdbda",
+  },
+  {
+    row: 9,
+    title: "Body Grooming",
+    description: "Only women shave beneath the neck.",
+    color: "#fdf5a1",
+  },
+];
